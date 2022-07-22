cbff3189a4340cc2317923df05f16aef7a06a291
parth.shah <parth.shah@msbc.in>
2022-07-22 15:52:18 +0530

    image upload will done automatically.

--- test.py
+++ test.py
@@ -12,13 +12,15 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.theverge.com/")
 driver.maximize_window()
 driver.implicitly_wait(10)
-for i in range(2, 10):
+for i in range(2, 22):
     driver.find_element(By.XPATH, f'(//h2[@class="c-entry-box--compact__title"]//a)[{i}]').send_keys(Keys.CONTROL,
                                                                                                      Keys.ENTER)
     driver.implicitly_wait(2)
     driver.switch_to.window(driver.window_handles[1])
     driver.implicitly_wait(2)
     title = driver.find_element(By.XPATH, '//h1[@class="c-page-title"]').text
+    if title == 'The Great Fiction of AI':
+        continue
     content = driver.find_element(By.XPATH, '//div[@class="c-entry-content "]').text
     with open('post_img.png', 'wb') as file:
         img = driver.find_element(By.XPATH, '//picture[@class="c-picture"]/img')
@@ -40,7 +42,8 @@ for i in range(2, 10):
     driver.implicitly_wait(2)
     driver.find_element(By.XPATH, '//input[@id="title"]').send_keys(title)
     driver.implicitly_wait(2)
-    driver.find_element(By.XPATH, '//label[@for="img_file"]').click()
+    driver.find_element(By.XPATH, '//input[@id="img_file"]').send_keys(
+        r"C:\Users\parth.shah\Desktop\tests\automation\post_img.png")
     driver.implicitly_wait(2)
     driver.find_element(By.XPATH, '//textarea[@id="content"]').send_keys(content)
     driver.find_element(By.XPATH, '//input[@value="Add Post"]').click()
